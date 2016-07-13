6e18c4cd0f06e326a68e9018eb29fb4cbcf53fb7
CamiloSalvo <camilo.salvo@usach.cl>
2016-07-13 02:42:29 -0400

    Fix combobox 2

--- app/controllers/cita_controller.rb
+++ app/controllers/cita_controller.rb
@@ -8,6 +8,7 @@ class CitaController < ApplicationController
 
   		@centros = Database.obtener_centros()
   		@especialidades = Database.obtener_especialidades()
+  		@medicos = Database.obtener_medicos()
 
   		@registrar_culiao= Rol.registrar_paciente(current_rol.run,current_rol.comuna,'1961-06-16',current_rol.sexo,current_rol.direccion,current_rol.num,current_rol.email,current_rol.prevision,1,current_rol.nombre,current_rol.apellidop,current_rol.apellidom,current_rol.run_dig_ver)
 
--- app/models/database.rb
+++ app/models/database.rb
@@ -13,7 +13,7 @@ class Database < ActiveRecord::Base
 	end
 
 	def self.obtener_medicos()
-		sql = "SELECT * FROM medico"
+		sql = "SELECT p.id_persona, p.nombre_persona, p.apellidop_persona, p.apellidom_persona FROM medico m, persona p WHERE m.id_persona = p.id_persona"
 		resultado = connection.execute(sql)
 		return resultado
 	end
