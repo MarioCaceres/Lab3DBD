c2dfe037447ecfdba05922c381adc23698c61673
CamiloSalvo <camilo.salvo@usach.cl>
2016-07-13 01:56:33 -0400

    Fix combobox

--- app/controllers/cita_controller.rb
+++ app/controllers/cita_controller.rb
@@ -1,11 +1,16 @@
 class CitaController < ApplicationController
+
 	before_filter :authenticate_rol!
-  def ver_citas
-  	#@citas = Rol.citas_por_rut(current_rol.run)
-  	@citas = Rol.citas_por_rut(14970293)
-  	puts "AAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-  	puts current_rol.comuna
-  	@registrar_culiao= Rol.registrar_paciente(current_rol.run,current_rol.comuna,'1961-06-16',current_rol.sexo,current_rol.direccion,current_rol.num,current_rol.email,current_rol.prevision,1,current_rol.nombre,current_rol.apellidop,current_rol.apellidom,current_rol.run_dig_ver)
-    
-  end
+
+  	def ver_citas
+
+  		@citas = Rol.citas_por_rut(14970293)
+
+  		@centros = Database.obtener_centros()
+  		@especialidades = Database.obtener_especialidades()
+
+  		@registrar_culiao= Rol.registrar_paciente(current_rol.run,current_rol.comuna,'1961-06-16',current_rol.sexo,current_rol.direccion,current_rol.num,current_rol.email,current_rol.prevision,1,current_rol.nombre,current_rol.apellidop,current_rol.apellidom,current_rol.run_dig_ver)
+
+  	end
+
 end
--- app/models/database.rb
+++ app/models/database.rb
@@ -1,2 +1,21 @@
 class Database < ActiveRecord::Base
+	
+	def self.obtener_centros()
+		sql = "SELECT * FROM centro_de_salud"
+		resultado = connection.execute(sql)
+		return resultado
+	end
+
+	def self.obtener_especialidades()
+		sql = "SELECT * FROM especialidad"
+		resultado = connection.execute(sql)
+		return resultado
+	end
+
+	def self.obtener_medicos()
+		sql = "SELECT * FROM medico"
+		resultado = connection.execute(sql)
+		return resultado
+	end
+
 end
