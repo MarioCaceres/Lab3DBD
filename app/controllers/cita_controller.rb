d9f02d1c93c1aae1b94ccf47f0a8494ec1e97c07
Mario Caceres <mario.caceres@usach.cl>
2016-07-12 22:44:23 -0400

    Panel de control2

--- app/controllers/cita_controller.rb
+++ app/controllers/cita_controller.rb
@@ -1,7 +1,8 @@
 class CitaController < ApplicationController
 	before_filter :authenticate_rol!
   def ver_citas
-  	@citas = Rol.citas_por_rut(current_rol.run)
+  	#@citas = Rol.citas_por_rut(current_rol.run)
+  	@citas = Rol.citas_por_rut(14970293)
 
   	#@registrar_culiao= Rol.registrar_paciente("11223344",'Cerrillos','1961-06-16',"1","calle_Falsa",'666',"fake_mail@usach.cl","banmedica","4","nombre_falso","apellidop_p","apellidom_p","5")
     
